4973e53e169988e167714315458d847b35edc762
GAGANDEEP SINGH <gagan.rishi999@gmail.com>
2019-10-12 14:05:27 -0400

    'Added folder for reqeusts'

---
+++ https.java
@@ -0,0 +1,51 @@
+public class https{
+
+    private static boolean isVerbose = false;
+
+    public static void main(String args[]) {
+        if ( args.length == 0){
+            System.out.println("\nEnter https help to get more information.\n");
+        }else{
+            cmdParser(args);
+        }
+        if (needHelp) {
+            help();
+        }else if(isGetRequest){
+            get(url);
+        }else if (isPostRequest){
+            post(url);
+        }
+    }
+
+    /**
+     * This method takes the cmd args and parses them according to the different conditions of the application.
+     * @param args an array of the command line arguments.
+     */
+    public static void cmdParser(String[] args){
+        for (int i =0; i<args.length; i++){
+            if (args[i].equalsIgnoreCase("-v")){
+                isVerbose = true;
+            }else if (args[i].equalsIgnoreCase("-h")){
+                hasHeaderData = true;
+                headerData = headerData.concat(args[i+1]+"\r\n");
+                i++;
+            }else if (args[i].equalsIgnoreCase("-d")){
+                hasInLineData = true;
+                inLineData = (args[i+1]);
+                i++;
+            }else if (args[i].equalsIgnoreCase("-f")){
+                readFromFile = true;
+                filePath = (args[i+1]);
+                i++;
+            }else if (args[i].equalsIgnoreCase("get")){
+                isGetRequest = true;
+            }else if (args[i].equalsIgnoreCase("post")){
+                isPostRequest = true;
+            }else if (args[i].equalsIgnoreCase("help")){
+                needHelp = true;
+            }else{
+                url = (args[i]);
+            }
+       }
+    }
+}
